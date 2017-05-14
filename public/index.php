9c4a9ee426abef9ed200f74a17260ffd2f35a82b
piranon <piranon.n@gmail.com>
2017-05-14 12:21:54 +0700

    Refactor code, create controller to handle request

--- public/index.php
+++ public/index.php
@@ -1,17 +1,16 @@
 <?php
 require '../vendor/autoload.php';
 
-use \Psr\Http\Message\ServerRequestInterface as Request;
-use \Psr\Http\Message\ResponseInterface as Response;
+use Slim\App;
+use LogFileViewer\Controller\FileController;
 
 $env = getenv('APP_ENV') ? : 'dev';
 
 $settings = require __DIR__ . "/../app/setting/settings.$env.php";
-$app = new \Slim\App($settings);
+$app = new App($settings);
 
 // Fetch DI Container
 $container = $app->getContainer();
-
 $container['notFoundHandler'] = function () {
     return function ($request, $response) {
         return $response->withJson(['error' => [
@@ -21,15 +20,5 @@ $container['notFoundHandler'] = function () {
     };
 };
 
-$app->get('/files[/{filePath:.*}]', function (Request $request, Response $response) {
-    $filePath = $request->getAttribute('filePath');
-    $segments = explode('/', $filePath);
-    if (!isset($segments[0]) || $segments[0] !== 'var' || !isset($segments[1]) || $segments[1] !== 'tmp') {
-        return $response->withJson(['error' => [
-            'code' => 400,
-            'message' => 'Path file must under /var/tmp'
-        ]], 400);
-    }
-});
-
+$app->get('/files[/{filePath:.*}]', FileController::class . ':getContentAction');
 $app->run();
---
+++ src/Controller/FileController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace LogFileViewer\Controller;
+
+use Psr\Http\Message\ServerRequestInterface as Request;
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Container\ContainerInterface;
+
+/**
+ * Class FileController
+ * @package LogFileViewer\Controller
+ */
+class FileController
+{
+    /**
+     * @var ContainerInterface
+     */
+    protected $container;
+
+    /**
+     * FileController constructor.
+     * @param ContainerInterface $container
+     */
+    public function __construct(ContainerInterface $container) {
+        $this->container = $container;
+    }
+
+    /**
+     * @param Request $request
+     * @param Response $response
+     * @return mixed
+     */
+    public function getContentAction(Request $request, Response $response) {
+        $filePath = $request->getAttribute('filePath');
+        $segments = explode('/', $filePath);
+        if (!isset($segments[0]) || $segments[0] !== 'var' || !isset($segments[1]) || $segments[1] !== 'tmp') {
+            return $response->withJson(['error' => [
+                'code' => 400,
+                'message' => 'Path file must under /var/tmp'
+            ]], 400);
+        }
+    }
+}
